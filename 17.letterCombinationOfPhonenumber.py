133cdbb518a85d91262c8c43d9d9b54b68dcea91
sruthikareddy9 <surrenderreddy06@gmail.com>
2026-05-04 17:43:39 +0530

    Create 17.letterCombinationOfPhonenumber.py

---
+++ 17.letterCombinationOfPhonenumber.py
@@ -0,0 +1,15 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+       if not  digits:
+        return[]
+       phone={"2":"abc","3":"def","4":"ghi","5":"jkl","6":"mno","7":"pqrs","8":"tuv","9":"wxyz"}
+       res=[]
+       def backtrack(index,path):
+        if index==len(digits):
+            res.append(path)
+            return
+        letters=phone[digits[index]]
+        for ch in letters:
+            backtrack(index+1,path+ch)
+       backtrack(0,"")
+       return res
