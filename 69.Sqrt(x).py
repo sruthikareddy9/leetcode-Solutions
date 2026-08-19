6d5fd1a47614d69b24870dc8d846eaf2cd9a78cf
sruthikareddy9 <surrenderreddy06@gmail.com>
2026-08-19 17:27:32 +0530

    Refactor mySqrt function for improved readability

--- 69.Sqrt(x).py
+++ 69.Sqrt(x).py
@@ -1,21 +1,15 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        if x < 2:
+        if x<2:
             return x
-
-        left, right = 1, x // 2
-        answer = 0
-
-        while left <= right:
-            mid = (left + right) // 2
-            square = mid * mid
-
-            if square == x:
-                return mid
-            elif square < x:
-                answer = mid
-                left = mid + 1
+        low=1
+        high=x
+        ans=1
+        while(low<=high):
+            mid=(low+high)//2
+            if((mid*mid)<=x):
+                ans=mid
+                low=mid+1
             else:
-                right = mid - 1
-
-        return answer
+                high=mid-1
+        return ans
