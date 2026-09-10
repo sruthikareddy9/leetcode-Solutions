3ccf9779c3d89f41be5c6214ba54bd0505747bba
sruthikareddy9 <surrenderreddy06@gmail.com>
2026-09-10 17:33:31 +0530

    Create 100.Same Tree.py

---
+++ 100.Same Tree.py
@@ -0,0 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        dq=deque([(p,q)])
+        while dq:
+            a,b=dq.popleft()
+            if not a and not b:
+                continue
+            if not a or not b:
+                return False
+            if a.val!=b.val:
+                return False
+            dq.append((a.left,b.left))
+            dq.append((a.right,b.right))
+        return True
+             
+
+        
