6107b06e29299d3668d74d800be2099db294b340
sruthikareddy9 <surrenderreddy06@gmail.com>
2026-09-15 17:31:20 +0530

    Add isSymmetric method to Solution class
    Implement a function to check if a binary tree is symmetric.

---
+++ 101.Symmetric Tree.py
@@ -0,0 +1,17 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isSymmetric(self, root: Optional[TreeNode]) -> bool:
+        def mirror(left,right):
+            if left is None and right is None:
+                return True
+            if left is None or right is None:
+                return False
+            if left.val!=right.val:
+                return False
+            return mirror(left.left,right.right) and mirror(left.right,right.left)
+        return mirror(root.left, root.right)
